cd15845481b2a4e2dba640c30358bd3b9de6afdc
sabrinaboani <sabrinapboani@gmail.com>
2025-09-01 21:32:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById('mensagem');
+    mensagem.textContent = "O desmatamento é um problema sério que afeta o clima, a biodiversidade e a vida humana. Devemos agir agora!";
+}
